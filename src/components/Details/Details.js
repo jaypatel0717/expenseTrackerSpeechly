44f332266a07479318ddaf9849b40140615490df
jaypatel0717 <jaypatel0717@gmail.com>
2021-01-12 20:32:50 -0500

    add

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
 import { Doughunt } from "react-chartjs-2";
+import useTransactions from "../../useTransactions";
+
 import useStyles from "./styles";
 
 const Details = ({ title }) => {
   const classes = useStyles();
+  const { total, chartData } = useTransactions(title);
+
   return (
     <Card className={title === "Income" ? classes.income : classes.expense}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">$50</Typography>
-        {/* <Doughunt data="data" /> */}
+        {/* <Typography variant="h5">${total}</Typography>
+        <Doughunt data={chartData} /> */}
       </CardContent>
     </Card>
   );
