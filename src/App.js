4007d38f2c1f9a6cd0aed28d28c258090969510d
jaypatel0717 <jaypatel0717@gmail.com>
2021-01-17 21:06:00 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Details from "./components/Details/Details";
 import Main from "./components/Main/Main";
 import { Grid } from "@material-ui/core";
-import useStyle from "./styles";
+import useStyles from "./styles";
+
+import { SpeechState, useSpeechContext } from "@speechly/react-client";
+import {
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+} from "@speechly/react-ui";
 
 const App = () => {
-  const classes = useStyle();
+  const classes = useStyles();
+  const { speechState } = useSpeechContext();
+  const main = useRef(null);
+
+  const executeScroll = () => main.current.scrollIntoView();
+
+  useEffect(() => {
+    if (speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState]);
+
   return (
     <div>
       <Grid
         className={classes.grid}
         container
         spacing={0}
-        alignContent="center"
+        alignItems="center"
         justify="center"
+        style={{ height: "100vh" }}
       >
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
           <Details title="Expense" />
         </Grid>
+        <PushToTalkButtonContainer>
+          <PushToTalkButton />
+        </PushToTalkButtonContainer>
       </Grid>
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,6 +12,7 @@ import { ExpenseTrackerContext } from "../../context/context";
 import useStyles from "./styles";
 import Form from "./Form/Form";
 import List from "./List/List";
+import InfoCard from "../InfoCard";
 
 const Main = () => {
   const classes = useStyles();
@@ -28,8 +29,7 @@ const Main = () => {
             variant="subtitle1"
             style={{ lineHeight: "1.5em", marginTop: "20px" }}
           >
-            {/* {InfoCard} */}
-            try saying
+            <InfoCard />
           </Typography>
           <Divider />
           <Form />
